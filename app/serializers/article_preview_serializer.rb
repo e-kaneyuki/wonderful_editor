6dc06d0c608c3f8e2cda8fe5694bc29f05542adf
e-kaneyuki <occultchikin@gmail.com>
2020-10-27 23:44:02 +0900

    各serializerのディレクトリ作成。AriticleControllerの編集を行った。 次はmodelとdatabaseの学習とそれぞれに値を入力するところまで進めたい。

---
+++ app/serializers/article_preview_serializer.rb
@@ -0,0 +1,7 @@
+class ArticlePreviewSerializer < ActiveModel::Serializer
+  #articlの一覧表示の処理だけは他の処理と分けたいので、別途ArticlePreviewSerializerを作成した！
+  attributes :id, :tittle, :updated_at
+  belongs_to :user, serializer: Api::V1::UserSerializer
+
+  has_many :comments, :article_likes
+end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -28,38 +28,6 @@
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #  index_users_on_uid_and_provider      (uid,provider) UNIQUE
 #
-require "rails_helper"
-
-RSpec.describe User, type: :model do
-  context "必要な情報が揃っている場合" do
-    let(:user) { build(:user) }
-
-    it "ユーザー登録できる" do
-      expect(user).to be_valid
-    end
-  end
-
-  context "名前のみ入力している場合" do
-    let(:user) { build(:user, email: nil, password: nil) }
-
-    it "エラーが発生する" do
-      expect(user).not_to be_valid
-    end
-  end
-
-  context "email がない場合" do
-    let(:user) { build(:user, email: nil) }
-
-    it "エラーが発生する" do
-      expect(user).not_to be_valid
-    end
-  end
-
-  context "password がない場合" do
-    let(:user) { build(:user, password: nil) }
-
-    it "エラーが発生する" do
-      expect(user).not_to be_valid
-    end
-  end
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :name, :email
 end
