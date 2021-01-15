9ec917a9ff4c9d0b1cae86ae1a6c92b316d44156
e-kaneyuki <occultchikin@gmail.com>
2021-01-15 21:59:51 +0900

    記事の削除の実装をしました

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -44,6 +44,10 @@ module Api::V1
 
       render json: @article, each_serializer: ArticleSerializer
     end
+    def destroy
+      @article = Article.find(params[:id])
+      @article.delete
+    end
 
     private
 
