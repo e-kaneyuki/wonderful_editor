d9cf9e8d540b59290bc99bf23925de10b81d6666
e-kaneyuki <occultchikin@gmail.com>
2021-03-12 23:18:50 +0900

    devise_tokne_authで設定されているヘッダーを受け取ることができる

--- spec/requests/api/v1/auth/registrations_request_spec.rb
+++ spec/requests/api/v1/auth/registrations_request_spec.rb
@@ -4,41 +4,94 @@ RSpec.describe "Api::V1::Auth::Registrations", type: :request do
 
   describe "POST/registrations" do
     #主題 = subject(subjectをベースとしたテストを作ってみよう！ってヤツ)
+
+############################### 正常系テスト ###############################
     subject { post(api_v1_user_registration_path, params: params) }
 
     context "リクエストが通っている場合" do
-      # binding.pry
       let (:params) { attributes_for(:user) }
-      # binding.pry
-      fit "レスポンスが返ってくる" do
-        # binding.pry
-        # expect(response).to have_http_status 200
-        expect { subject }.to change { User.count }.by(1)
 
+      it "ユーザーが登録される" do
+        expect { subject }.to change { User.count }.by(1)
+        expect(response).to have_http_status(200)
+      end
+    end
+###### 真のゴールがなんなのかを考える！！！######
+    context "headerをキチンと受け取れる" do
+      let (:params) { attributes_for(:user) }
+      it "header 情報を取得することができる" do
+        subject
+        header = response.header
+        expect(header["access-token"]).to be_present
+        expect(header["client"]).to be_present
+        expect(header["expiry"]).to be_present
+        expect(header["uid"]).to be_present
+        expect(header["token-type"]).to be_present
+      end
+    end
+
+
+###########################################
+
+
+    context "Name/Emailが入力されている" do
+      let (:params) { attributes_for(:user) }
+      it "登録ができる" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res["data"]["name"]).to eq params[:name]
+        expect(response).to have_http_status(200)
+      end
+
+      it "名前が登録される" do
+        subject
+        res = JSON.parse(response.body)
+        expect(res["data"]["email"]).to eq params[:email]
+        expect(response).to have_http_status(200)
       end
     end
 
 
 
-    # context "適切なパラメーターが入力されている" do
-    #   # binding.pry
-    #   let (:params) { { user: attributes_for(:user) } }
-    #   # binding.pry
-    #   fit "レスポンスが返ってくる" do
-    #     # binding.pry
-    #     expect(response.status).to eq(200)
-    #     expect { subject }.to change { User.count }.by(1)
+############################### 異常系テスト ###############################
 
-    #   end
-    # end
+    context "適切な名前が入力されていない" do
+      let (:params) { attributes_for(:user, name: nil) }
+      it "新規登録ができない" do
+        subject
 
-    #パラメーターが許可されているか否か
-
-
-    # パラメーターが重複していないか
+        res = JSON.parse(response.body)
+        expect(res["data"]["name"]).to eq params[:name]
+        expect(response).to have_http_status(422)
+        expect(res["errors"]["name"]).to include "can't be blank"
+      end
+    end
 
 
-    # パラメーターの記号がルールに沿っているか(大文字が小文字か)(記号の可否)
+    context "適切なメールアドレスが入力されていない" do
+      let (:params) { attributes_for(:user, email: nil) }
+      it "新規登録ができない" do
+        subject
+        res = JSON.parse(response.body)
+
+        expect(res["data"]["email"]).to eq params[:email]
+        expect(response).to have_http_status(422)
+        expect(res["errors"]["email"]).to include "can't be blank"
+      end
+    end
+
+
+    context "適切なpasswordが入力されていない" do
+      let (:params) { attributes_for(:user, password: nil) }
+      it "新規登録ができない" do
+        subject
+        res = JSON.parse(response.body)
+
+        expect(res["data"]["password"]).to eq params[:password]
+        expect(response).to have_http_status(422)
+        expect(res["errors"]["password"]).to include "can't be blank"
+      end
+    end
 
   end
 end
