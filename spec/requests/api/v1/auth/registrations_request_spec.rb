d9efb0634da7b472900a10eaad1cf53ed475724f
e-kaneyuki <occultchikin@gmail.com>
2021-03-13 16:06:08 +0900

    RSpecでのexpect数の上限を4→5に変更した

--- spec/requests/api/v1/auth/registrations_request_spec.rb
+++ spec/requests/api/v1/auth/registrations_request_spec.rb
@@ -1,25 +1,25 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Auth::Registrations", type: :request do
-
   describe "POST/registrations" do
-    #主題 = subject(subjectをベースとしたテストを作ってみよう！ってヤツ)
+    # 主題 = subject(subjectをベースとしたテストを作ってみよう！ってヤツ)
 
-############################### 正常系テスト ###############################
+    ############################### 正常系テスト ###############################
     subject { post(api_v1_user_registration_path, params: params) }
 
     context "リクエストが通っている場合" do
-      let (:params) { attributes_for(:user) }
+      let(:params) { attributes_for(:user) }
 
       it "ユーザーが登録される" do
         expect { subject }.to change { User.count }.by(1)
-        expect(response).to have_http_status(200)
+        expect(response).to have_http_status(:ok)
       end
     end
-###### 真のゴールがなんなのかを考える！！！######
+    ###### 真のゴールがなんなのかを考える！！！######
+
     context "headerをキチンと受け取れる" do
-      let (:params) { attributes_for(:user) }
-      it "header 情報を取得することができる" do
+      let(:params) { attributes_for(:user) }
+      it "header情報を取得することができる" do
         subject
         header = response.header
         expect(header["access-token"]).to be_present
@@ -30,68 +30,61 @@ RSpec.describe "Api::V1::Auth::Registrations", type: :request do
       end
     end
 
-
-###########################################
-
+    ###########################################
 
     context "Name/Emailが入力されている" do
-      let (:params) { attributes_for(:user) }
+      let(:params) { attributes_for(:user) }
       it "登録ができる" do
         subject
         res = JSON.parse(response.body)
         expect(res["data"]["name"]).to eq params[:name]
-        expect(response).to have_http_status(200)
+        expect(response).to have_http_status(:ok)
       end
 
       it "名前が登録される" do
         subject
         res = JSON.parse(response.body)
         expect(res["data"]["email"]).to eq params[:email]
-        expect(response).to have_http_status(200)
+        expect(response).to have_http_status(:ok)
       end
     end
 
-
-
-############################### 異常系テスト ###############################
+    ############################### 異常系テスト ###############################
 
     context "適切な名前が入力されていない" do
-      let (:params) { attributes_for(:user, name: nil) }
+      let(:params) { attributes_for(:user, name: nil) }
       it "新規登録ができない" do
         subject
 
         res = JSON.parse(response.body)
         expect(res["data"]["name"]).to eq params[:name]
-        expect(response).to have_http_status(422)
+        expect(response).to have_http_status(:unprocessable_entity)
         expect(res["errors"]["name"]).to include "can't be blank"
       end
     end
 
-
     context "適切なメールアドレスが入力されていない" do
-      let (:params) { attributes_for(:user, email: nil) }
+      let(:params) { attributes_for(:user, email: nil) }
       it "新規登録ができない" do
         subject
         res = JSON.parse(response.body)
 
         expect(res["data"]["email"]).to eq params[:email]
-        expect(response).to have_http_status(422)
+        expect(response).to have_http_status(:unprocessable_entity)
         expect(res["errors"]["email"]).to include "can't be blank"
       end
     end
 
-
     context "適切なpasswordが入力されていない" do
-      let (:params) { attributes_for(:user, password: nil) }
+      let(:params) { attributes_for(:user, password: nil) }
       it "新規登録ができない" do
         subject
         res = JSON.parse(response.body)
 
         expect(res["data"]["password"]).to eq params[:password]
-        expect(response).to have_http_status(422)
+        expect(response).to have_http_status(:unprocessable_entity)
         expect(res["errors"]["password"]).to include "can't be blank"
       end
     end
-
   end
 end
